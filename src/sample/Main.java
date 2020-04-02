52cd3e49678af09c8d85be8423fe692be0a200a9
hcorupe <harrisoncorupe0@gmail.com>
2020-04-01 17:29:28 -0700

    DataBase init

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,16 @@
 package sample;
 
+import com.mysql.cj.protocol.Resultset;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends Application {
 
@@ -18,6 +24,46 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/tictactoe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=PST";
+        String username = "test";
+        String password = "test123password";
+        Connection myConn;
+
+        try{
+            myConn = DriverManager.getConnection(url,username,password);
+            Statement myStatement = myConn.createStatement();
+            System.out.println("JDBC Database Connected");
+
+            String sql = "select * from tictactoe.user";
+            ResultSet rs = myStatement.executeQuery(sql);
+
+            while(rs.next()){
+                System.out.println(rs.getString("username"));
+            }
+
+            myConn.close();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+
         launch(args);
     }
 }
+//jdbc:mysql://localhost/db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC
+/*
+try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/sonoo","root","root");
+//here sonoo is database name, root is username and password
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select * from emp");
+        while(rs.next())
+        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+        con.close();
+        }catch(Exception e){ System.out.println(e);}
+
+ */
