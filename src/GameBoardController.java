7c3206f0e94fce13bed32a806238b7b0488f9060
alexYamaoka <ryamaoka1@gmail.com>
2020-04-04 15:50:21 -0700

    added gamboard controller

---
+++ src/GameBoardController.java
@@ -0,0 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class GameBoardController
+{
+    @FXML
+    Label player1Name, player2Name;
+
+    @FXML
+    Label playerOverallStatsWins, playerOverallStatsLosses, playerOverallStatsTies;
+
+    @FXML
+    Label currentGameStatsWins, currentGameStatsLosses, currentGameStatsTies;
+
+    @FXML
+    Label showChatHistory, userChatInput;
+
+    @FXML
+    Button userChatEnter, mainMenu, newGame, startNewChat;
+
+
+
+    @FXML
+    Label panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8, panel9;
+    
+
+
+
+
+}
