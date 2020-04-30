00f06c002a797e20b25b9db8cff5e68758d3d468
hcorupe <harrisoncorupe0@gmail.com>
2020-04-29 20:40:12 -0700

    Database

--- src/GameService/GameService.java
+++ src/GameService/GameService.java
@@ -65,12 +65,13 @@ public class GameService implements Runnable, Service
     public void addGame(GameRoomInformation game){
         ongoingGameRooms.put(game.getGameName(),game);
     }
+
     public GameRoomInformation getGame(String Id){
         return ongoingGameRooms.get(Id);
     }
+
     public Set<String> getGames(){
         return ongoingGameRooms.keySet();
-
     }
 
     public void handle(ClientConnection clientConnection, Packet packet) {
