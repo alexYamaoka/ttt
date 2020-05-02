4fb927c3540c1b9abcdc609681c23333df18296f
justin-blanco <blancorjustin@gmail.com>
2020-05-01 17:30:17 -0700

    play button changes scenes to game lobby

--- src/UI/Client/GameLobby.java
+++ src/UI/Client/GameLobby.java
@@ -1,6 +1,23 @@
 package UI.Client;
 
-public class GameLobby {
+import Client.ClientController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameLobby implements Initializable {
+
+    private ClientController clientController;
+
+    public void setClientController(ClientController clientController) {
+        this.clientController = clientController;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
 
 }
