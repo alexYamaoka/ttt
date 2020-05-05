3621c38cd802cf2571215eb018182778d1b1ca83
justin-blanco <blancorjustin@gmail.com>
2020-05-05 12:27:19 -0700

    Reformatted Server Package

--- src/Server/ClientConnection.java
+++ src/Server/ClientConnection.java
@@ -1,4 +1,5 @@
 package Server;
+
 import Shared.Packet;
 import Shared.UserInformation;
 
@@ -7,14 +8,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientConnection implements Runnable {
+    private final AtomicBoolean running = new AtomicBoolean(false);
     private Socket socket;
     private Server.Service service;
-
-    private final AtomicBoolean running = new AtomicBoolean(false);
     private ObjectOutputStream output;
     private ObjectInputStream input;
     private UserInformation information;
@@ -36,10 +35,10 @@ public class ClientConnection implements Runnable {
             output = new ObjectOutputStream(socket.getOutputStream());
             input = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
 
-            while(running.get()) {
-               Packet packet = (Packet) input.readObject();     // retrieves userinformation thats updated from account service
-               information = packet.getInformation();
-               service.handle(this, packet);
+            while (running.get()) {
+                Packet packet = (Packet) input.readObject();     // retrieves userinformation thats updated from account service
+                information = packet.getInformation();
+                service.handle(this, packet);
             }
         } catch (IOException | ClassNotFoundException ex) {
             stop();
@@ -58,8 +57,7 @@ public class ClientConnection implements Runnable {
         return output;
     }
 
-    public ObjectInputStream getInputStream()
-    {
+    public ObjectInputStream getInputStream() {
         return input;
     }
 
