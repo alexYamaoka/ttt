bdd1b2e176d33113be064f893b8d5940286ab20f
YuShuRChen <a0936582936@gmail.com>
2020-05-11 22:33:32 -0700

    added button to gamehistorystats

--- src/UI/Client/GameHistoryStatsController.java
+++ src/UI/Client/GameHistoryStatsController.java
@@ -1,12 +1,21 @@
 package UI.Client;
 
+import Client.ClientController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 
 public class GameHistoryStatsController {
     @FXML
     Label gameID, player1Name, player2Name, startTime, endTime, result, zZ, zO, zT, oZ, oO, oT, tZ, tO, tT;
+    @FXML
+    Button returnToGHButton;
+
+    private ClientController clientController;
 
     public void importGameInformation(String gameID, String player1Name, String player2Name, String startTime, String endTime, String result, String zZ, String zO, String zT, String oZ, String oO, String oT, String tZ, String tO, String tT){
         this.gameID.setText(gameID);
@@ -24,5 +33,17 @@ public class GameHistoryStatsController {
         this.tZ.setText(tZ);
         this.tO.setText(tO);
         this.tT.setText(tT);
+    }
+
+    public void returnToGHButtonClicked(ActionEvent event) {
+        Stage stage = null;
+        Parent root = null;
+
+        if(event.getSource() == returnToGHButton) {
+            stage = (Stage) returnToGHButton.getScene().getWindow();
+            root = clientController.getGameHistoryPane();
+        }
+        stage.setScene(root.getScene());
+        stage.show();
     }
 }
