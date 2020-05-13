f83a615b47aa77a10d5c36b6808d6a603c23ae3c
justin-blanco <blancorjustin@gmail.com>
2020-05-12 21:30:50 -0700

    fixed merge conflicts

--- src/ObserverPatterns/GameHistoryListener.java
+++ src/ObserverPatterns/GameHistoryListener.java
@@ -1,7 +1,12 @@
 package ObserverPatterns;
 
+import Models.Game;
 import Shared.GameInformation;
+
+import java.util.List;
 
 public interface GameHistoryListener {
     void updateGameHistory(GameInformation gameInformation);
+
+    void updateHistory(List<Game> list);
 }
